07cbf2eb46d7a513fc1b524f37f0104021d25233
Patrick Hulce <patrick.hulce@gmail.com>
2018-03-22 20:47:11 -0700

    feat: initial commit

---
+++ bin/hulk.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const gitRewrite = require('../lib/git-rewrite')
+
+const options = yargs
+  .command('git-rewrite', 'rewrite git dates', {
+    number: {alias: 'n', required: true},
+    hours: {alias: 'h'},
+    message: {alias: 'm'},
+  })
+  .demand(1).argv
+
+
+switch (options._[0]) {
+  case 'git-rewrite':
+    gitRewrite(options)
+    break
+  default:
+    throw new Error('Invalid command')
+}
