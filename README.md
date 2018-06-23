9df1939b0c92c5aa410bc2acfaf8b1b68495e0de
Patrick Hulce <patrick.hulce@gmail.com>
2018-06-22 17:36:05 -0700

    feat: add npm-publish script

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # @patrickhulce/scripts
+
 [![NPM Package](https://badge.fury.io/js/hulk.svg)](https://www.npmjs.com/package/@patrickhulce/lint)
 [![Build Status](https://travis-ci.org/patrickhulce/hulk.svg?branch=master)](https://travis-ci.org/patrickhulce/hulk)
 [![Coverage Status](https://coveralls.io/repos/github/patrickhulce/hulk/badge.svg?branch=master)](https://coveralls.io/github/patrickhulce/hulk?branch=master)
-[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 [![Dependencies](https://david-dm.org/patrickhulce/hulk.svg)](https://david-dm.org/patrickhulce/hulk)
 
 Collection of useful scripts.
--- bin/hulk.js
+++ bin/hulk.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
+const npmPublish = require('../lib/npm-publish')
 const gitRewrite = require('../lib/git-rewrite')
 const tinyImage = require('../lib/tiny-image')
 
 const options = yargs
-  .command('git-rewrite', 'rewrite git dates', {
-    number: {alias: 'n', required: true},
-    hours: {alias: 'h'},
+  .command('npm-publish', 'publish to npm and GitHub and such', {
+    lerna: {},
+    prerelease: {type: 'boolean'},
+    branch: {alias: 'b', default: 'master'},
+    'node-version': {default: 'v8'},
+    'dry-run': {type: 'boolean'},
+  })
+  .command('git-rewrite', 'rewrite git metadata', {
+    number: {alias: 'n', required: true, type: 'number'},
+    hours: {alias: 'h', type: 'number'},
     message: {alias: 'm'},
-    'dry-run': {default: true},
+    'dry-run': {type: 'boolean'},
   })
   .command('tiny-image', 'shrink image to inlineable size', {
     output: {alias: 'o'},
@@ -19,6 +27,9 @@ const options = yargs
 
 async function go() {
   switch (options._[0]) {
+    case 'npm-publish':
+      await npmPublish(options)
+      break
     case 'git-rewrite':
       await gitRewrite(options)
       break
